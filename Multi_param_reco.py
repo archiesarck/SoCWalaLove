3f819cf4b29acdf84d5056b86b5f722a24020314
Archies Arck <37809902+arkches@users.noreply.github.com>
2018-07-03 11:34:29 +0530

    Add files via upload

--- Multi_param_reco.py
+++ Multi_param_reco.py
@@ -7,42 +7,46 @@ import sys
 from numpy import *
 import mysql.connector
 
-uid = int(sys.argv[1])
-tid = str(sys.argv[2])
-
+#uid = int(sys.argv[1])
+#tid = str(sys.argv[2])
+uid = 14
+tid = str(1)
+curr_user_index_ratings = 0
+curr_user_index_params = 0
 num_videos=0
 num_users=0
 
 cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1', database='soc')
+#cursors :)====================
+cursor_num_users = cnx.cursor()
+cursor_num_videos = cnx.cursor()
+cursor_ratings = cnx.cursor()
+cursor_params_matrix = cnx.cursor()
+cursor_user_params = cnx.cursor()
+#=================================
 
-cursor_1 = cnx.cursor()
-cursor_2 = cnx.cursor()
-query_1 = ("select ID from users")
+query_num_users = ("select ID from users")
 
-cursor_1.execute(query_1) 
+cursor_num_users.execute(query_num_users) 
 
-for(ID) in cursor_1:
+for(UID) in cursor_num_users:
     #print(ID,Username)
     num_users = num_users+1
-cursor_1.close() 
 
-query_2 = ("select VID from videos")
-cursor_2.execute(query_2) 
+#total number of users
 
-for(VID,Name) in cursor_2:
+cursor_num_users.close() 
+
+query_num_videos = ("select VID from videos")
+cursor_num_videos.execute(query_num_videos) 
+
+for(VID) in cursor_num_videos:
     #print(VID,Name)
     num_videos = num_videos+1
-cursor_2.close()        
-cnx.close()
 
-# In[2]:
+#total number of videos
 
-#initialize number of videos and users
-
-
-
-# In[3]:
-
+cursor_num_videos.close()
 
 #initialize attributes
 num_params=6
@@ -53,28 +57,73 @@ num_params=6
 
 
 #create ratings matrix from database
-ratings = random.randint(6, size = (num_videos, num_users), dtype=int)
+#old ratings matrix 
+#=================================================================
+#ratings = random.randint(6, size = (num_videos, num_users), dtype=int)
+#ratings
+#commenting it out!
+#=================================================================
+#new ratings
+#=================================================================
+#take ratings from ratings table in the db!
+
+query_ratings = ("select * from ratings")
+cursor_ratings.execute(query_ratings)
+ratings = [[0 for i in range(num_videos)] for j in range(num_users)]
+
+#ratings matrix is like:
+#       video1 video2 video3 .........................
+#user1     0     0      0
+#user2     0     0      0
+#user3     0     0      0
+#.
+#.
+#.
+#it will be fetched by ratings[user][video]
+
+result = cursor_ratings.fetchall()
+for row in result:
+    for i in range(num_users):
+        if(row[0]==uid):curr_user_index_ratings = i
+        #assigning index of current user which is in ratings matrix.
+        #i.e. the position of row in which ratings of current user is present
+        for j in range(num_videos):
+            ratings[i][j] = row[j+1]
 ratings
-
-
+cursor_ratings.close()
+#ratings table updated 
+#=================================================================
 # In[5]:
 
 
 #create params_matrix 
+#old params_matrix, I will comment this out!
+#=================================================================
 params_matrix = random.random(size=(num_videos,num_params))
 params_matrix
+#=================================================================
+#new params_matrix after taking values from db!
+#=================================================================
+query_params_matrix = ("select difficulty,relevance,complexity,length,production,engaging from videos")
+cursor_params_matrix.execute(query_params_matrix)
+result_params_matrix = cursor_params_matrix.fetchall()
 
-for i in range(num_videos):
-    for j in range(num_params):
-        print(params_matrix[i][j])
+params_matrix = [[0 for x in range(num_params)] for y in range(num_videos)]
+for row in result_params_matrix:
+    for i in range(num_videos):
+        for j in range(num_params):
+            params_matrix[i][j] = row[j]
 params_matrix
+cursor_params_matrix.close()
+#params_matrix updated with values from db!
+#=================================================================
 
 # In[7]:
 
 
 #convert params_matrix to unit vectors
 for video in range(num_videos):
-    norm=linalg.norm(params_matrix[video])
+    norm=linalg.norm(params_matrix[video,:])
     params_matrix[video][:] = [x / norm for x in params_matrix[video]]
 params_matrix
 
@@ -83,12 +132,27 @@ params_matrix
 
 
 #load user params
-user_params = random.random(size=(num_users,num_params))
+#old user_params, I will comment this out!
+#=================================================================
+#user_params = random.random(size=(num_users,num_params))
+#user_params
+#=================================================================
+#new user_params after taking values from db!
+#=================================================================
+user_params = [[0 for x in range(num_params)] for y in range(num_users)]
+query_user_params = ("select ID,difficulty,relevance,complexity,length,production,engaging from users")
+cursor_user_params.execute(query_user_params)
+result_user_params = cursor_user_params.fetchall()
+for row in result_user_params:
+    for i in range(num_users):
+        if(row[0]==uid):curr_user_index_params = i
+        #assigning curr_user_index_params to be used to find cost!
+        for j in range(num_params):
+            user_params[i][j] = row[j+1]
 user_params
-
-
-# In[9]:
-
+cursor_user_params.close()
+#updated user_params from table users!
+#=================================================================
 
 #convert user_params to unit vectors
 
@@ -104,7 +168,7 @@ user_params
 #rated matrix tells whether a particular movie is rated or not
 rated_matrix=(ratings!=0)
 rated_matrix
-
+#same structure as ratings matrix!
 
 # In[11]:
 
@@ -114,14 +178,23 @@ rated_matrix
 #to current topic
 #such that ratings[user_index][video_index[i]]=0 implies nhy7
 #USER is new to the topic
-curr_user_index=0 #uid
-curr_topic=None   #tid
-video_index=[]
+
+#curr_user_index = already assigned
+
+#we can fetch the index of array row which consists of this 
+#uid and the use it in line 177 because it is not neccesary that curr_user_index
+#will always be equal to that index, it can be discontinuous!
+
+#curr_topic is replaced by "tid" which we are getting from php file!
+
+video_index = []
 for i in range(num_videos):
-    if rated_matrix[i][curr_user_index]==False:
+    if rated_matrix[curr_user_index_ratings][i]==False:
         video_index.append(i)
 video_index
-    
+
+if(size(video_index)==0):print("No videos to recommend!")
+#nothing to recommend, user have already seen all of then :)    
 
 
 # In[12]:
@@ -161,7 +234,7 @@ def calc_user_var(curr_user,user_params):
 # In[14]:
 
 
-user_var = calc_user_var(curr_user_index,user_params)
+user_var = calc_user_var(curr_user_index_params,user_params)
 user_var
 
 
@@ -181,14 +254,16 @@ p=0
 for video in video_index:
     count=0
     for user in range(num_users):
-        if user == curr_user_index:
+        if user == curr_user_index_ratings:
             continue
         else:
-            video_rated[p] += ratings[video][user]*user_val[user]
-            if rated_matrix[video][user] == True:
+            video_rated[p] += ratings[user][video]*user_val[user]
+            if rated_matrix[user][video] == True:
                 count+=1
     video_rated[p] /= count
     p+=1
 
 video_rated
 
+#close the db connection!
+cnx.close()  
